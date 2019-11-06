9a2b1952ab0ab523c21d1e80c3e754ebd2a2e253
Ismail Ashour <iashour@extreme.sa>
2019-11-06 13:35:26 +0200

    wip

--- src/Aramex.php
+++ src/Aramex.php
@@ -1,20 +1,14 @@
 <?php
 
-
 namespace DigitalCloud\Aramex;
 
-
-use DigitalCloud\Aramex\API\Requests\Pickup;
+use DigitalCloud\Aramex\API\Requests\PickupCreation;
 use DigitalCloud\Aramex\API\Requests\Rating;
 use DigitalCloud\Aramex\API\Requests\Shipping;
 use DigitalCloud\Aramex\API\Requests\Tracking;
 
 class Aramex
 {
-    /**
-     * @param bool $live
-     * @return Rating
-     */
     public static function ratting()
     {
         return new Rating();
@@ -27,7 +21,7 @@ class Aramex
 
     public static function pickup()
     {
-        return new Pickup();
+        return new PickupCreation();
     }
 
     public static function tracking()
